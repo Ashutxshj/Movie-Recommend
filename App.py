522ada6397faf18bf08dae0bb6896171dfcfb13d
Ashutosh Jha <129574087+Ashutxshj@users.noreply.github.com>
2024-06-23 21:56:24 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import difflib
-from scikit-learn.feature_extraction.text import TfidfVectorizer
-from scikit-learn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 movies_data = pd.read_csv(r'movies.csv')
